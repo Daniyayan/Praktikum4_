9375ca98067f3c9dee5772e1502c88277f91563d
Daniyayan <94636137+Daniyayan@users.noreply.github.com>
2021-12-02 23:47:47 +0800

    Add files via upload

---
+++ P4_2_1204016/Form1.Designer.cs
@@ -0,0 +1,67 @@
+﻿namespace P4_2_1204016
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.openFileDialog = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // openFileDialog
+            // 
+            this.openFileDialog.Location = new System.Drawing.Point(276, 126);
+            this.openFileDialog.Name = "openFileDialog";
+            this.openFileDialog.Size = new System.Drawing.Size(248, 39);
+            this.openFileDialog.TabIndex = 0;
+            this.openFileDialog.Text = "Use the Open file Dialog Control";
+            this.openFileDialog.UseVisualStyleBackColor = true;
+            this.openFileDialog.Click += new System.EventHandler(this.openFileDialog_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.openFileDialog);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button openFileDialog;
+    }
+}
+
